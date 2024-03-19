9b28fe7644848ecc222c1a4fc46de477662179c6
TomoriComAncora <lucas.fernando@ufms.br>
2024-03-19 11:38:36 -0400

    Avançando

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,14 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      sans: ["Roboto", "sans-serif"],
+    },
     extend: {
-      backgroundImage:{
-        "home": "url('/assets/bg.png')" //declara o nome que vai usar e passa o caminho;
-      }
+      backgroundImage: {
+        home: "url('/assets/bg.png')", //declara o nome que vai usar e passa o caminho;
+      },
     },
   },
   plugins: [],
-}
-
+};
